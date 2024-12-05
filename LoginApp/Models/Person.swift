4ce9ea5a32f2ba23b68e47799cc094e4ef4b4339
Yuri Luka <lukayurii.work@gmail.com>
2024-12-05 20:13:22 +0500

    added getPerson method

--- LoginApp/Models/Person.swift
+++ LoginApp/Models/Person.swift
@@ -6,3 +6,16 @@
 //  Copyright © 2024 Alexey Efimov. All rights reserved.
 //
 
+struct Person {
+    let name: String
+    let surname: String
+    let age: Int
+    let email: String
+}
+
+extension Person {
+    static func getPerson() -> Person {
+        let person = Person(name: "Yurii", surname: "Luka", age: 28, email: "yurii@gmail.com")
+        return person
+    }
+}
