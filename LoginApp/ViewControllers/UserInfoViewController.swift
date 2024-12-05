7079bc712357322aa8be6792a70e62c7c381e416
Yuri Luka <lukayurii.work@gmail.com>
2024-12-05 21:03:16 +0500

    Added UserInfoVC

---
+++ LoginApp/ViewControllers/UserInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserInformationViewController.swift
+//  LoginApp
+//
+//  Created by Yurii Luka on 05.12.2024.
+//  Copyright © 2024 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
