ab3b5f94d05af26380370f350e31cf90477ba3e1
Yuri Luka <lukayurii.work@gmail.com>
2024-12-06 20:03:17 +0500

    Work with UserBioVC

---
+++ LoginApp/ViewControllers/UserBioViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UserDescriptionViewController.swift
+//  LoginApp
+//
+//  Created by Yurii Luka on 05.12.2024.
+//  Copyright © 2024 Yurii Luka. All rights reserved.
+//
+
+import UIKit
+
+class UserBioViewController: UIViewController {
+    //MARK: - IBOuntlets
+    @IBOutlet var userBioLabel: UILabel!
+    
+    //MARK: - Ovarride methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - Private methods
+
+}
